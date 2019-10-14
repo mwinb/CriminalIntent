f16fe3add49e97a87e6234247badffee9be827a4
mwinb <mwinberry0101@gmail.com>
2019-10-14 13:51:20 -0700

    Pg 177. Chapter 8

--- app/src/main/java/edu/osucascades/cs492/criminalintent/CrimeFragment.java
+++ app/src/main/java/edu/osucascades/cs492/criminalintent/CrimeFragment.java
@@ -33,7 +33,7 @@ public class CrimeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        View v = inflater.inflate(R.layout.crime_fragment, container, false);
 
         mTitleField = (EditText) v.findViewById(R.id.crime_title);
         mTitleField.addTextChangedListener(new TextWatcher() {
--- app/src/main/java/edu/osucascades/cs492/criminalintent/CrimeListActivity.java
+++ app/src/main/java/edu/osucascades/cs492/criminalintent/CrimeListActivity.java
@@ -2,10 +2,10 @@ package edu.osucascades.cs492.criminalintent;
 
 import androidx.fragment.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
