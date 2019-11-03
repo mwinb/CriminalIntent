5113363fb4a23c82155f8fba07e953d3a299b721
mwinb <mwinberry0101@gmail.com>
2019-11-03 11:18:34 -0800

    Chapter 11 Finished

--- app/src/main/java/edu/osucascades/cs492/criminalintent/CrimeListFragment.java
+++ app/src/main/java/edu/osucascades/cs492/criminalintent/CrimeListFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -74,7 +73,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
         }
 
